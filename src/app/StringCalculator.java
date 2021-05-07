735d896526bb23c5ba5215e830a3669cb9be424b
Tarun <45634649+tarun18k@users.noreply.github.com>
2021-05-07 10:00:32 +0530

    Basic Calculator..

---
+++ src/app/StringCalculator.java
@@ -0,0 +1,23 @@
+package app;
+public class StringCalculator {
+public int add(String numbers)
+{
+	if(numbers.equals("")) {
+		return 0;
+	}
+	else {
+		String delimiter=",";
+		
+		String numberlist[]=numbers.split(","+"|\n");
+		return sum(numberlist);
+	}
+}
+private int sum(String[] numbers) {
+	int total=0;
+	for(String number:numbers)
+	{
+		total+=Integer.parseInt(number);
+	}
+	return total;
+}
+}
